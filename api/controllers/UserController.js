6e1fbaa18107599c27201d0be44c91160cab0036
Alex Wilson <acwilson96@protonmail.com>
2017-10-02 00:35:59 +0100

    CRUD Skeleton for UserController.

---
+++ api/controllers/UserController.js
@@ -0,0 +1,27 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    create: (req, res) => {
+        
+    },
+
+    read: (req, res) => {
+        
+    },
+
+    update: (req, res) => {
+        
+    },
+
+    delete: (req, res) => {
+        
+    }
+	
+};
+
