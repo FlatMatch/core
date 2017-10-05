9010ef14896c4d77d635f767bd80037945b6c7a4
Alex Wilson <acwilson96@protonmail.com>
2017-10-05 15:49:50 +0100

    Added authToken generation, and attach a Device to a User.

--- api/models/Device.js
+++ api/models/Device.js
@@ -5,10 +5,31 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+const crypto = require('crypto');
+
 module.exports = {
 
   attributes: {
 
+    owner: {
+      model: 'User'
+    },
+
+    authToken: {
+      type: 'longtext',
+      required: true
+    },
+
+  },
+
+  /* Generate a random authToken */
+  beforeCreate: (values, cb) => {
+    crypto.randomBytes(256, (err, buf) => {
+      if (err) cb(err);
+      values.token = buf.toString('hex');
+      cb();
+    });
   }
+
 };
 
